7901bd8e7e705628decf45353af61c4b312c20e2
shivam <shivampoddartech@gmail.com>
2025-04-03 13:12:12 +0530

    Array Practice Set

---
+++ Student.java
@@ -0,0 +1,12 @@
+package AarayPracticeQuestion;
+
+public class Student {
+	public static void main(String[] args) {
+		int [] marks= { 70,80,90,65,74,45};
+		for (Object obj : marks) {
+			System.out.println( "Student Marks "+ obj);
+	}
+}
+
+}
+
